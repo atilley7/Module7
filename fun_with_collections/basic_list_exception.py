9d7d7cca1fcb412aeff0429edf2d3dc4f6fad21e
atilley7 <averytilley7@gmail.com>
2020-10-12 18:36:55 -0500

    all pass

--- fun_with_collections/basic_list_exception.py
+++ fun_with_collections/basic_list_exception.py
@@ -1,15 +1,19 @@
 def make_list():
     myList = []
     for x in range(3):
-        myList.insert(x, int(get_input()))
+        val = get_input()
+        if not val.isdecimal():
+            raise ValueError
+        elif int(val) < 1:
+            raise ValueError
+        elif int(val) > 50:
+            raise ValueError
+        else:
+            myList.insert(x, int(val))
+
     return myList
 
 
 def get_input():
     x = input("Input number : ")
-    if not x.isdecimal():
-        raise Exception('Please enter only a number')
-    else:
-        return x
-
-
+    return x
