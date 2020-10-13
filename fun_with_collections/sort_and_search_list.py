95a3540611fe9858092e307b64c0af68ccc3442f
atilley7 <averytilley7@gmail.com>
2020-10-12 19:25:01 -0500

    1, 6 ,4 as input, item found passes becasue it sees 1 at postion 0. sort passes after rearranging list

--- fun_with_collections/sort_and_search_list.py
+++ fun_with_collections/sort_and_search_list.py
@@ -2,11 +2,11 @@ def search_list(new_list, value):
    for x in range(len(new_list)):
     if new_list[x] == value:
         return x
-    return -1
+    return -1 # needs return statements to show if the value was int the list
 
 
 
 
 
 def sort_list(new_list):
-    pass
+    new_list.sort() # needs no input , just sorts list
--- test_collections/test_sort_and_search_list.py
+++ test_collections/test_sort_and_search_list.py
@@ -6,10 +6,13 @@ from fun_with_collections.basic_list_exception import make_list
 
 class MyTestCase(unittest.TestCase):
     def test_item_found(self):
-        self.assertEqual(search_list(x, 1), 1)
+        self.assertEqual(search_list(x, 1), 0)
 
     def test_item_not_found(self):
         self.assertEqual(search_list(x, 66), -1)
+
+    def test_sort_list(self):
+        self.assertEqual(sort_list(x), [1, 4, 6])
 
 
 x = make_list()
