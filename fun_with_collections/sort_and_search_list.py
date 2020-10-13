89c0778a50bacbbaa0a682279381ef77c2f09487
atilley7 <averytilley7@gmail.com>
2020-10-12 19:15:18 -0500

    switched to using make list. to pass test 1 needs to be the second input

--- fun_with_collections/sort_and_search_list.py
+++ fun_with_collections/sort_and_search_list.py
@@ -1,5 +1,11 @@
-def search_list(new_list, val):
-    pass
+def search_list(new_list, value):
+   for x in range(len(new_list)):
+    if new_list[x] == value:
+        return x
+    return -1
+
+
+
 
 
 def sort_list(new_list):
